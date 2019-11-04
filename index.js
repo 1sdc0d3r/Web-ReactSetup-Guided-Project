0cb71ce526d1b5f9336417629bc3b54f02b019c0
Braden <fish2355@hotmail.com>
2019-11-03 22:05:25 -0700

    step 7 done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import './src/App';
+import "./src/App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>H3110 W0r1d</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector("#target"));
